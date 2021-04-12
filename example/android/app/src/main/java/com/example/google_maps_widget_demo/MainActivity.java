e68997971836698fb22092318cb1a96707211260
Rithik Bhandari <rithikbhandari895@gmail.com>
2021-04-12 16:43:50 +0530

    added example app

---
+++ example/android/app/src/main/java/com/example/google_maps_widget_demo/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.google_maps_widget_demo;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ example/lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_widget/google_maps_widget.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          body: GoogleMapsWidget(
+            apiKey: "YOUR KEY HERE",
+            sourceLatLng: LatLng(40.484000837597925, -3.369978368282318),
+            destinationLatLng: LatLng(40.48017307700204, -3.3618026599287987),
+            routeWidth: 2,
+            sourceMarkerIconInfo: MarkerIconInfo(
+              assetPath: "assets/images/house-marker-icon.png",
+            ),
+            destinationMarkerIconInfo: MarkerIconInfo(
+              assetPath: "assets/images/restaurant-marker-icon.png",
+            ),
+            driverMarkerIconInfo: MarkerIconInfo(
+              assetPath: "assets/images/driver-marker-icon.png",
+              assetMarkerSize: Size.square(125),
+            ),
+            // mock stream
+            driverCoordinatesStream: Stream.periodic(
+              Duration(milliseconds: 500),
+                  (i) => LatLng(
+                40.47747872288886 + i / 10000,
+                -3.368043154478073 - i / 10000,
+              ),
+            ),
+            onTap: (l) => print(l),
+            sourceName: "This is source name",
+            driverName: "Alex",
+            onTapDriverMarker: (currentLocation) {
+              print("Driver is currently at $currentLocation");
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
